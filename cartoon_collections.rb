073ad8bbdc1375d91b7772c5760499c5a48d41c1
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-09 22:38:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,14 +7,12 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.map do |element|
-    element.first.upcase
-    element + "!"
-  end
+
 end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? {|word| word.length > 4}
 end
 
 def find_the_cheese(array)# code an argument here
