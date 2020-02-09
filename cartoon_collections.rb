b180cf71329bc12661c45b702e190d66ec9a6744
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-09 22:26:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index do |dwarf, num|
     puts "#{num + 1}. #{dwarf}"
+  end
 end
 
 def summon_captain_planet(array)# code an argument here
