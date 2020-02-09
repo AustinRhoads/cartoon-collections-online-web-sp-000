7cfe1218881350550064530b6e621cd41a3b0c7e
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-09 23:16:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.map {|i| "#{i.split("").first.upcase.join("")}!"}
+  array.map {|i| "#{i[0].upcase}!"}
 
 end
 
