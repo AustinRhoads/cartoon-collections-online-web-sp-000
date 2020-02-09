5b5d8cbf3450b4406c768664eee1ccb6329a5c96
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-09 22:29:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,6 +7,10 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map do |element|
+    element.first.upcase
+    element + "!"
+  end
 end
 
 def long_planeteer_calls(array)# code an argument here
