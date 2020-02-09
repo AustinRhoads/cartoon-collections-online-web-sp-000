430410add43c47a205a0bbf7cd30c973f67f082e
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-09 23:34:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,8 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-array.each do |i|
-  i.select?(cheese_types.include?(i))
+array.collect do |i|
+  cheese_types.include?(i)
 end
 
 end
