fa4bd01354d95dacd7daaedd5ca3049a685f7d63
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-09 23:11:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,6 +7,7 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map {|i| "#{i.upcase}!"}
 
 end
 
@@ -18,7 +19,6 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-array.map do |i|
-  cheese_types.include?(i)
-end
+
+
 end
